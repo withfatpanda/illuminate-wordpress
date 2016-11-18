5dcda5ff54b6b41bbd80cee08de7d468a1ec5e79
Aaron Collegeman <aaron@collegeman.net>
2016-11-17 23:07:29 -0500

    init

---
+++ src/Example.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace FatPanda\Illuminate\WordPress;
+
+class Example
+{
+
+}
---
+++ tests/ExampleTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace FatPanda\Illuminate\WordPress\Tests;
+
+class ExampleTest extends \PHPUnit_Framework_TestCase
+{
+
+}
